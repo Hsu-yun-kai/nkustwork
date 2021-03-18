45ebe54bc19bee20e7c78a726163576464c97284
HsuYunKai <yunkai0518@gmail.com>
2021-03-18 11:57:40 +0800

    撰寫程式

--- work/work/Program.cs
+++ work/work/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using work.Services;
 
 namespace work
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
+            var xmlService = new ImpotXmlservice();   //搬動藍圖到基底程式
+
+            var datas = xmlService.LoadFormFile(@"C:\Users\user\OneDrive\文件\nkustwork\docs\臺中市路邊停車收費路段.xml");
+
+
+
             Console.WriteLine("Hello World!");
             
         }
---
+++ work/work/Services/ImpotXmlservice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work.Services
+{
+    public class ImpotXmlservice
+    {
+        /*private int count; //全域變數        
+        public ImpotXmlservice(int count) 
+        {
+            this.count = count;
+        }*/
+
+        public ImpotXmlservice() { }
+        public  List<Object> LoadFormFile(string filePath)      //給一個檔案路徑回一個陣列
+            {
+                //System.IO.FileInfo fi = new System.IO.FileInfo(filePath);   //開啟檔案
+                //fi.~~~
+
+                var str = System.IO.File.ReadAllText(filePath);
+
+                var xDocument =  System.Xml.Linq.XDocument.Parse(str);                   //靜態讀取
+                //System.Xml.Linq.XDocument xDocument = new System.Xml.Linq.XDocument();    
+
+                var targets = xDocument.Descendants("StationExit");
+
+            
+
+                return null;
+            }
+    }
+}
